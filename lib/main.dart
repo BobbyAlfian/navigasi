293d0d7cc1dab1c513989027b36f1b5df70ab45f
BobbyAlfian <hmdbam11@gmail.com>
2023-05-25 21:09:11 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Navigation")),
+      body: Column(
+        children: [
+          const Text("Ini Halaman Pertama"),
+          ElevatedButton(onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));
+          }, 
+          child: const Text("Pindah Halaman 2")),
+        ],
+      ),
+    );
+  }
+}
